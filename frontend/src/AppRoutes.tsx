d89c2765b52fde1ad3e57ea55414b81ce7484d74
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2024-07-17 23:44:03 -0300

    Protecting profile-page

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,13 +3,16 @@ import { Layout } from "./layouts/layout"
 import HomePage from "./pages/HomePage"
 import AuthCallbackPage from "./pages/AuthCallbackPage"
 import UserProfilePage from "./pages/UserProfilePage"
+import ProtectedRoute from "./auth/ProtectedRoute"
 
 const AppRoutes = () => {
   return(
     <Routes>
       <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
       <Route path="/" element={<Layout><HomePage/></Layout>}/>
-      <Route path="/user-profile" element={<Layout displayHero={false} ><UserProfilePage/></Layout>}/>
+      <Route element={<ProtectedRoute/>}>
+        <Route path="/user-profile" element={<Layout displayHero={false} ><UserProfilePage/></Layout>}/>
+      </Route>
       <Route path="*" element={<Navigate to="/" />}/>
     </Routes>
   )
